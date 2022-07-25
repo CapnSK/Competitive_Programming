5faba2cee2b512c58f06be6bc57e81eb99ae406c
Sankalp Kulkarni <kulkarnisankalp21@gmail.com>
2022-07-25 15:54:58 +0530

    BUYSWEET solution as per editorial

--- CODECHEF/Practice_Codechef/BUYSWEET.py
+++ CODECHEF/Practice_Codechef/BUYSWEET.py
@@ -1,67 +1,27 @@
 from functools import cmp_to_key
 import math
+from pydoc import doc
 
 def run(debug=True):
     for _ in range(int(input())):
         n,r = list(map(int, input().split()))
         cost = list(map(int, input().split()))
         discount = list(map(int, input().split()))
-        
-        costEffectiveDict = {}
+        discountedCost = []
         for i in range(n):
-            if cost[i] not in costEffectiveDict:
-                costEffectiveDict[cost[i]] = discount[i]
-            else:
-                costEffectiveDict[cost[i]] = max(costEffectiveDict[cost[i]], discount[i])
-        costEffectiveSorted = sorted(list(costEffectiveDict.items()), key = cmp_to_key(comparator))    
-        if debug:
-            print("cost effective dict is", costEffectiveDict)
-            print("cost effective sorted dict is", costEffectiveSorted)
-        
-        totalItemsBought=0
-        i=0
-        while i<len(costEffectiveSorted):
-            if costEffectiveSorted[i][0] > r:
-                i+=1
-                if debug:
-                    print("incrementing i from first if",i)
+            discountedCost.append([cost[i]-discount[i] ,i])
+        discountedCost.sort(key=lambda x:x[0])
+        ans=0
+        for i in range(n):
+            [currDiscountedCost, ind] = discountedCost[i]
+            currCost = cost[ind]
+            currDiscount = discount[ind]
+            if currCost>r:
                 continue
-            else:
-                if debug:
-                    print("numerator",r-costEffectiveSorted[i][0])
-                    print("denominator",costEffectiveSorted[i][0]-costEffectiveSorted[i][1])
-                itemsBoughtInThisIteration=(math.ceil((r-costEffectiveSorted[i][0])/(costEffectiveSorted[i][0]-costEffectiveSorted[i][1])))
-                if itemsBoughtInThisIteration >=0:
-                    remMoney=r-(itemsBoughtInThisIteration*(costEffectiveSorted[i][0]-costEffectiveSorted[i][1]))
-                    if debug:
-                        print("remMoney",remMoney)
-                    itemsBoughtInThisIteration += 1 if remMoney>= costEffectiveSorted[i][0] else 0
-                    totalItemsBought+=itemsBoughtInThisIteration
-                    r-=(itemsBoughtInThisIteration*(costEffectiveSorted[i][0]-costEffectiveSorted[i][1]))
-                if debug:
-                    print("totalItemsBought",totalItemsBought)
-                    print("itemsBoughtInThisIteration",itemsBoughtInThisIteration)
-                    print("Iteration",i)
-                    print("r",r)
-                # itemsBought = ((r-costEffectiveSorted[i][0])//(costEffectiveSorted[i][1] or costEffectiveSorted[i][0]))+1
-                # totalItemsBought+=itemsBought
-                # r-=(itemsBought*(costEffectiveSorted[i][0]-costEffectiveSorted[i][1]))
-                i+=1
-                if debug:
-                    print("incrementing i from else",i)
-                continue
-        if debug:
-            print("totalItemsBought",totalItemsBought)
-            print("i",i)
-        print(totalItemsBought)
-            
-
-
-def comparator(item1, item2):
-    if (item1[0]-item1[1])-(item2[0]-item2[1])!=0:
-        return (item1[0]-item1[1])-(item2[0]-item2[1])
-    else:
-        return -1 if item1[0] < item2[0] else 1
+            bought=((r-currDiscount)//(currDiscountedCost))
+            ans+= bought
+            r-=bought*currDiscountedCost
+        print(ans)
   
 if __name__ == '__main__':
     while True:
